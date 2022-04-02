ecc0d1a5b6950d87b7ff56def86766fb0e829f51
Ahmad Mateen <ahmadmateen786@gmail.com>
2022-04-02 21:46:29 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+echo "01-04-2022".'<br/>';
+echo "Variables and Constants".'<br/>';
+// Variabls
+$name="Johan Smith".'<br/>';
+echo $name;
+// Constants
+// By Using Define funcation
+define('NAME','Ahmad');
+echo NAME.'<br/>';
+// By Using Const keyword
+const fName="Shahid Mateen".'<br>';
+echo fName;
+// Predefine Constants
+echo PHP_VERSION.'<br/>';
+// Majic Constants
+echo __FILE__;    
+
+//------------------------
+echo "<br/>";
+echo "02-04-2022".'<br/>';
+echo "Data Types in Php".'<br/>';
+echo "1-Boolen".'<br/>';
+echo "2-Integer".'<br/>';
+echo "3-Flot".'<br/>';
+echo "4-String".'<br/>';
+echo "5-Arrys".'<br/>';
+echo "6-Objects".'<br/>';
+echo "7-Callable".'<br/>';
+echo "8-Iterabl".'<br/>';
+echo "9-Rsourcs".'<br/>';
+echo "10-Null".'<br/>';
+// 
+?>
