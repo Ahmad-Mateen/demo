ad95b476eefd6457fb601d52cb847922177ef5d7
Ahmad Mateen <ahmadmateen786@gmail.com>
2022-04-05 21:00:07 +0500

    updated

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ echo $name;
 define('NAME','Ahmad');
 echo NAME.'<br/>';
 // By Using Const keyword
-const fName="Shahid Mateen".'<br>';
-echo fName;
+const fname="Shahid Mateen".'<br>';
+echo fname;
 // Predefine Constants
 echo PHP_VERSION.'<br/>';
 // Majic Constants
 echo __FILE__;
 echo __LINE__;
-    
+
 
 //------------------------
 echo "<br/>";
@@ -35,25 +35,80 @@ echo "10-Null".'<br/>';
 //
 echo "3-4-2022".'<br/>';
 //Arrays In PhP
-$Person=['Johan','Face','Hands','Mouth'];
+$person=['Johan','Face','Hands','Mouth'];
 echo '<pre>';
-print_r($Person);
+print_r($person);
 echo '<pr/>';
 //for counting of array Element
-echo 'Total Number of Arrays = '.count($Person);
+echo 'Total Number of Arrays = '.count($person);
 // we can add more elements by push function
-array_push($Person,'Eyes','Teeth');
+array_push($person,'Eyes','Teeth');
 echo '<pre>';
-print_r($Person);
+print_r($person);
 echo '<pr/>';
 //Associative Array
-$Languages=[
-    'Php'=> '1.8',
-    'Java'=> '1.8',
-    'Larvl'=> '1.8',
+$languages=[
+    'php'=> '1.8',
+    'java'=> '1.8',
+    'larvl'=> '1.8',
 ];
 echo '<pre>';
-print_r($Languages);
+print_r($languages);
 echo '<pr/>';
-
+echo "4-4-2022".'<br/>';
+//Operators
+//Arithmetic Operators
+$operators=["+","-","*","/"];
+print_r($operators);
+//if else statements
+$number=20;
+if($number==20)
+{
+    echo"The Given number is equal to 20.";
+}
+else
+{
+    echo "Not Matched";
+}
+// loops
+//For Loop
+echo 'With For Loop'.'<br/>';
+for($i=0;$i<=10;$i++)
+{
+    echo 'The Number is= '. $i.'<br/>';
+}
+// do-While loop
+echo '----------------'.'<br/>';
+echo 'With For  do While Loop'.'<br/>';
+$p=0;
+do
+{
+    
+    echo 'The Number is= '. $p.'<br/>'; 
+    $p++;
+}while($p<=5); 
+//
+echo '----------------'.'<br/>';
+echo 'With For While loop  '.'<br/>';
+$n=0;
+while($n<=5)
+{
+    echo 'The Number is= '. $n.'<br/>'; 
+    $n++;
+}
+// Switch Statement
+$message='paid';
+switch ($message) {
+    case 'paid':
+        echo 'Status Paid.';
+        break;
+    case 'declined':
+        echo 'declind';
+        break;
+        case 'pending':
+            echo 'Pending';
+            break;
+    default:
+        echo 'Unknown Payment Status.';
+}
 ?>
