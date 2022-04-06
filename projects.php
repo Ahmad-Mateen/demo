2ca157f0fdb750fe14f6c3f9ffbc6ad963cb99e4
Ahmad Mateen <ahmadmateen786@gmail.com>
2022-04-06 20:39:53 +0500

    Updatd

--- projects.php
+++ projects.php
@@ -1,6 +1,7 @@
-// Palindrome Finder
+
 <?php
-if(isset($_POST["submit"]))
+// With Reverse function
+/*if(isset($_POST["submit"]))
 {
     $n=$_POST["word"];
     $revers=strrev($n);
@@ -12,10 +13,35 @@ if(isset($_POST["submit"]))
     {
         echo 'The Given Word is not Palindrome.';
     }
-}
+}*/
 ?>
 <form method="post">
     <h1>Plaindrom Finder</h1>
-    <input type="textbox" name="word" reuired/>
+    <input type="textbox" name="value" required/>
     <input type="submit" name="submit"/>
+    <?php
+        if(isset($_POST['submit']))
+        {
+            $word="";
+            $result="";
+            $word=$_POST['value'];
+            $length=strlen($word) -1;
+            
+            for($i=$length; $i>=0; $i--)
+            {
+                $result.=$word[$i];
+        
+            }
+            //echo $result;
+            if($word==$result)
+            {
+                echo '<h5>The given word is plaindrom</h5>';
+            }
+            else
+        {
+            echo '<h5>The given word is not plaindrom</h5>';
+        }
+        }
+
+    ?>
 </form>
